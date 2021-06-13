dc0bd2c74212d00a5c07f4871b9f523737ae689f
ksbrimbon <ksbrimbon@yahoo.com>
2021-06-14 02:05:03 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,8 +1,10 @@
+#create a function that is for caching the mean of the vetor
+#in this case matrix is the vector
 makeCacheMatrix <- function(x = matrix()){
   inv <- NULL      #inverse value printed as NULL
   set <- function(y){
     x <<- y
-    inv <<- NULL
+    inv <<- NULL   #the inverse is set to NULL
   }
   get <- function (){x}   #function to get matrix x
   setInverse <- function (inverse) {inv <<- inverse}
@@ -13,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()){
 cacheSolve <- function(x, ...){     ##get the cache data
   inv <- x$getInverse()
   if(!is.null(inv)){      #to check if the inverse value is null
-    message("getting cached data")
+    message("getting cached data")   #message prompt that will appear
     return(inv)         #returns the inverse value
   }
   mat <- x$get()
